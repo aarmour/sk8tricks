8e28fc090d67c7bc3c2a9cf7b21d6802f187559b
Adam Armour <adam.armour@gmail.com>
2015-08-23 16:45:50 -0600

    chore: initial commit

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+var config = require('hjs-webpack');
+
+module.exports = config({
+  // Entry point
+  in: 'src/js/app.js',
+
+  // Output directory
+  out: '_site',
+
+  // Destroy and re-create the output directory
+  clearBeforeBuild: true,
+
+  // Don't generate index.html
+  html: false
+});
