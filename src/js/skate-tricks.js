2cb0a51b1c0e9affd4c9314524d8a574d86b6ae3
Adam Armour <adam.armour@gmail.com>
2015-08-30 14:57:16 -0600

    Add tricks and merge inverse keys into the root

--- src/js/skate-tricks.js
+++ src/js/skate-tricks.js
@@ -1,13 +1,41 @@
 let tricksInternal = {
+  Hardflip: {
+    'Late Backside 180': { name: 'Ghetto Bird', excludeReverse: true },
+    '360 Body Rotation, Backside': { name: 'Diamond Flip', excludeReverse: true }
+  },
+  Heelflip: {
+    'Pop Shuvit, Frontside': { name: 'Varial Heelflip', aliases: ['Heelflip Shuvit'] },
+    'Pop Shuvit, Backside': { name: 'Inward Heelflip' },
+    '360 Pop Shuvit, Frontside': { name: 'Laser Flip', aliases: ['Frontside 360 Heelflip'] },
+    '360 Pop Shuvit, Backside': { name: '360 Inward Heelflip', aliases: ['Mainecoon Flip'] }
+  },
   Kickflip: {
     'Pop Shuvit, Backside': { name: 'Varial Kickflip', aliases: ['Kickflip Shuvit', '180 Flip'] },
     'Pop Shuvit, Frontside': { name: 'Hardflip' },
-    '360 Pop Shuvit, Frontside': { name: 'Tre Flip', aliases: ['360 Flip'] }
-  },
-  Heelflip: {
-    'Pop Shuvit, Frontside': { name: 'Varial Heelflip', aliases: ['Heelflip Shuvit'] }
+    '360 Pop Shuvit, Backside': { name: 'Tre Flip', aliases: ['360 Flip'] },
+    '360 Pop Shuvit, Frontside': { name: '360 Hardflip'}
   }
 };
+
+mergeInverse();
+
+function mergeInverse() {
+  Object.keys(tricksInternal).forEach(function (key) {
+    var trick = tricksInternal[key];
+
+    Object.keys(trick).forEach(function (innerKey) {
+      var innerTrick = trick[innerKey];
+
+      if (innerTrick.excludeReverse) return;
+
+      if (!tricksInternal[innerKey]) {
+        tricksInternal[innerKey] = {};
+      }
+
+      tricksInternal[innerKey][key] = innerTrick;
+    });
+  });
+}
 
 /**
  * Return a list of fundamental tricks.
